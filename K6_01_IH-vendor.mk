566889ac8f9822a228b73aec2645356a104802ed
Iscle <albertiscle9@gmail.com>
2019-07-26 18:43:31 +0200

    Port to K6_01_IH

---
+++ K6_01_IH-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/K6_01_IH/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/K6_01_IH/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/K6_01_IH/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/K6_01_IH/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/K6_01_IH/proprietary/xbin,system/xbin)
+
